60c61a8bb0b235ea3b0e6aff10fbedf84abc4317
James Harris <jgbharris@gmail.com>
2024-10-04 10:54:06 +0100

    Chat now working between two users, layout formatted, channel list at the side

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,3 +1,55 @@
+"use client";
+
+import {
+  Chat,
+  Channel,
+  MessageList,
+  MessageInput,
+  ChannelHeader,
+  Window,
+  Thread,
+  LoadingIndicator,
+  ChannelList,
+} from "stream-chat-react";
+import { EmojiPicker } from "stream-chat-react/emojis";
+import useInitializeChatClient from "../hooks/useInitializeChatClient";
+import { useUser } from "@clerk/nextjs";
+
 export default function ChatPage() {
-  return <div>This is the chat</div>;
+  const chatClient = useInitializeChatClient();
+  const { user } = useUser();
+
+  if (!chatClient || !user) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <LoadingIndicator size={40} />;
+      </div>
+    );
+  }
+
+  return (
+    <div className="h-screen">
+      <Chat client={chatClient}>
+        <div className="flex h-full flex-row">
+          <div className="w-full max-w-[360px]">
+            <ChannelList
+              filters={{ type: "messaging", members: { $in: [user.id] } }}
+              sort={{ last_message_at: -1 }}
+              options={{ presence: true, state: true, limit: 10 }}
+            />
+          </div>
+          <div className="h-full w-full">
+            <Channel EmojiPicker={EmojiPicker}>
+              <Window>
+                <ChannelHeader />
+                <MessageList />
+                <MessageInput />
+              </Window>
+              <Thread />
+            </Channel>
+          </div>
+        </div>
+      </Chat>
+    </div>
+  );
 }
---
+++ src/app/hooks/useInitializeChatClient.ts
@@ -0,0 +1,49 @@
+import { useUser } from "@clerk/nextjs";
+import { env } from "@/env";
+import { useEffect, useState } from "react";
+import { StreamChat } from "stream-chat";
+
+export default function useInitializeChatClient() {
+  const { user } = useUser();
+  const [chatClient, setChatClient] = useState<StreamChat | null>(null);
+
+  useEffect(() => {
+    if (!user?.id) return;
+    const client = StreamChat.getInstance(env.NEXT_PUBLIC_STREAM_KEY);
+    client
+      .connectUser(
+        {
+          id: user.id,
+          name: user.fullName || user.id,
+          image: user.imageUrl,
+        },
+        async () => {
+          const response = await fetch(`/api/get-token`);
+          if (!response.ok) {
+            throw new Error("Failed to get Stream Chat token");
+          }
+          const body = await response.json();
+          return body.token;
+        }
+      )
+      .catch((error) => {
+        console.error("Failed to connect to Stream Chat", error);
+      })
+      .then(() => {
+        setChatClient(client);
+      });
+    return () => {
+      setChatClient(null);
+      client
+        .disconnectUser()
+        .catch((error) => {
+          console.error("Failed to disconnect from Stream Chat", error);
+        })
+        .then(() => {
+          console.log("Connection closed");
+        });
+    };
+  }, [user?.id, user?.fullName, user?.imageUrl]);
+
+  return chatClient;
+}
