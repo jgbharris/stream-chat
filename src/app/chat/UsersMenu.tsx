17800212ec360f0d3fcf6ecba51bf3ad9ae7e708
James Harris <jgbharris@gmail.com>
2024-10-26 15:29:29 +0100

    Adding in debounce hook and loading states for searching users

--- src/app/chat/UsersMenu.tsx
+++ src/app/chat/UsersMenu.tsx
@@ -8,6 +8,7 @@ import { UserResource } from "@clerk/types";
 import { Channel, UserResponse } from "stream-chat";
 import { ArrowLeft } from "lucide-react";
 import LoadingButton from "@/components/LoadingButton";
+import useDebounce from "@/hooks/useDebounce";
 
 interface UsersMenuProps {
   loggedInUser: UserResource;
@@ -24,20 +25,38 @@ export default function UsersMenu({
 
   const [users, setUsers] = useState<(UserResponse & { image?: string })[]>();
 
+  const [searchInput, setSearchInput] = useState("");
+
+  const searchInputDebounced = useDebounce(searchInput);
+
   const [moreUsersLoading, setMoreUsersLoading] = useState(false);
 
   const [endOfPaginationReached, setEndOfPaginationReached] =
     useState<boolean>();
 
-  const pageSize = 2;
+  const pageSize = 10;
 
   useEffect(() => {
     async function loadInitialUsers() {
-      // await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate loading
+      setUsers(undefined);
+      setEndOfPaginationReached(undefined);
+      await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate loading
       try {
         const response = await client.queryUsers(
           {
             id: { $ne: loggedInUser.id }, // Filter out the logged in user
+            ...(searchInputDebounced
+              ? {
+                  $or: [
+                    {
+                      name: { $autocomplete: searchInputDebounced },
+                    },
+                    {
+                      id: { $autocomplete: searchInputDebounced },
+                    },
+                  ],
+                }
+              : {}),
           },
           { id: 1 }, // Sort by id
           { limit: pageSize + 1 } // Load one more than the page size to check if there are more users
@@ -50,11 +69,11 @@ export default function UsersMenu({
       }
     }
     loadInitialUsers();
-  }, [client, loggedInUser.id]);
+  }, [client, loggedInUser.id, searchInputDebounced]);
 
   async function loadMoreUsers() {
     setMoreUsersLoading(true);
-    await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate loading
+    // await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate loading
     try {
       const lastUserId = users?.[users.length - 1].id;
       if (!lastUserId) {
@@ -63,7 +82,22 @@ export default function UsersMenu({
 
       const response = await client.queryUsers(
         {
-          $and: [{ id: { $ne: loggedInUser.id } }, { id: { $gt: lastUserId } }],
+          $and: [
+            { id: { $ne: loggedInUser.id } },
+            { id: { $gt: lastUserId } },
+            searchInputDebounced
+              ? {
+                  $or: [
+                    {
+                      name: { $autocomplete: searchInputDebounced },
+                    },
+                    {
+                      id: { $autocomplete: searchInputDebounced },
+                    },
+                  ],
+                }
+              : {},
+          ],
         },
         { id: 1 },
         { limit: pageSize + 1 } // Sort by id
@@ -98,26 +132,41 @@ export default function UsersMenu({
 
   return (
     <div className="str-chat absolute z-10 h-full w-full border-e border-e-[#DBDDE1] bg-white">
-      <div className="flex items-center gap-3 p-3 text-lg font-bold">
-        <ArrowLeft onClick={onClose} className="cursor-pointer" /> Users
-      </div>
-      <div>{!users && <LoadingUsers />}</div>
-      {users?.map((user) => (
-        <UserResult
-          key={user.id}
-          user={user}
-          onUserClicked={startChatWithUser}
+      <div className="flex flex-col p-3">
+        <div className="mb-3 flex items-center gap-3 p-3 text-lg font-bold">
+          <ArrowLeft onClick={onClose} className="cursor-pointer" /> Users
+        </div>
+        <input
+          type="search"
+          placeholder="Search"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          className="rounded-full border border-gray-300 px-4 py-2"
         />
-      ))}
-      {endOfPaginationReached === false && (
-        <LoadingButton
-          loading={moreUsersLoading}
-          className="m-auto mb-3 w-[80%]"
-          onClick={loadMoreUsers}
-        >
-          Load more users
-        </LoadingButton>
-      )}
+      </div>
+      <div>
+        {users?.map((user) => (
+          <UserResult
+            key={user.id}
+            user={user}
+            onUserClicked={startChatWithUser}
+          />
+        ))}
+        <div className="px-3">
+          {!users && !searchInputDebounced && <LoadingUsers />}
+          {!users && searchInputDebounced && "Searching..."}
+          {users?.length === 0 && <div>No users found</div>}
+        </div>
+        {endOfPaginationReached === false && (
+          <LoadingButton
+            loading={moreUsersLoading}
+            className="m-auto mb-3 w-[80%]"
+            onClick={loadMoreUsers}
+          >
+            Load more users
+          </LoadingButton>
+        )}
+      </div>
     </div>
   );
 }
