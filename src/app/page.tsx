cc5a0ac1b6cda190f379c608e1c4d0fd5804bdad
James Harris <jgbharris@gmail.com>
2024-12-16 01:47:47 +0000

    Landing page redesign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-gradient-to-tl from-gray-200 via-gray-100 to-black">
       <Image
-        src="/backchat_logo.png"
+        src="/back_chat_logo_transparent.png"
         width={800}
         height={800}
         alt="Back Chat Logo"
