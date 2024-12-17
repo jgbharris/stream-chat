4bb9b98a838abcc7d99eef331297bd61e0666984
James Harris <jgbharris@gmail.com>
2024-12-17 16:06:43 +0000

    Tweaking landing page sizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,30 +7,30 @@ export default function Home() {
     <div className="flex h-screen w-screen flex-col items-center justify-center bg-gradient-to-tl from-gray-200 via-gray-100 to-black">
       <Image
         src="/back_chat_logo_transparent.png"
-        width={600}
-        height={600}
+        width={550}
+        height={550}
         alt="Back Chat Logo"
       />
       <div className="flex">
         <Image
           src="/screenshot_light.png"
-          width={350}
-          height={350}
+          width={300}
+          height={300}
           alt="App in light theme"
           className="skew-y-6"
         />
         <Image
           src="/screenshot_dark.png"
-          width={350}
-          height={350}
+          width={300}
+          height={300}
           alt="App in dark theme"
           className="mx-20 skew-y-6"
         />
       </div>
 
-      <h1 className="my-20 text-4xl font-bold">
+      <h2 className="my-20 text-4xl font-bold">
         The new way to connect with friends
-      </h1>
+      </h2>
       <Button as={Link} href="/chat" className="w-[360px] font-bold">
         START CHATTING
       </Button>
