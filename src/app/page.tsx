0b5c3d1591342da7d2f894b9a66198d8f26a5b2e
James Harris <jgbharris@gmail.com>
2024-12-17 18:43:31 +0000

    Tweaking landing page for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,31 +4,35 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center bg-gradient-to-tl from-gray-200 via-gray-100 to-black">
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-gradient-to-tl from-gray-200 via-gray-100 to-black p-3">
       <Image
         src="/back_chat_logo_transparent.png"
         width={550}
         height={550}
         alt="Back Chat Logo"
       />
-      <div className="flex">
-        <Image
-          src="/screenshot_light.png"
-          width={300}
-          height={300}
-          alt="App in light theme"
-          className="skew-y-6"
-        />
-        <Image
-          src="/screenshot_dark.png"
-          width={300}
-          height={300}
-          alt="App in dark theme"
-          className="mx-20 skew-y-6"
-        />
+      <div className="m-3 flex h-screen w-screen items-center justify-center gap-2 sm:flex-col md:flex-col lg:flex-row">
+        <div>
+          <Image
+            src="/screenshot_light.png"
+            width={300}
+            height={300}
+            alt="App in light theme"
+            className="ml-2 skew-y-6"
+          />
+        </div>
+        <div>
+          <Image
+            src="/screenshot_dark.png"
+            width={300}
+            height={300}
+            alt="App in dark theme"
+            className="skew-y-6"
+          />
+        </div>
       </div>
 
-      <h2 className="my-20 text-4xl font-bold">
+      <h2 className="my-20 text-center text-4xl font-bold">
         The new way to connect with friends
       </h2>
       <Button as={Link} href="/chat" className="w-[360px] font-bold">
